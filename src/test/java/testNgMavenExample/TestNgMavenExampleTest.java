05efccc6c45dd3214188a20eb2ea21239f9cad1d
amreen.noor <amreen.noor@thomsonreuters.com>
2019-07-16 16:52:12 +0530

    adding

--- src/test/java/testNgMavenExample/TestNgMavenExampleTest.java
+++ src/test/java/testNgMavenExample/TestNgMavenExampleTest.java
@@ -87,7 +87,18 @@ public class TestNgMavenExampleTest {
 
 final ChromeOptions options = new ChromeOptions();
 		 
+options.addArguments("--test-type","ignore-certifcate-errors");
 
+options.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
+        options.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
+    //    options.addArguments("--headless"); // only if you are ACTUALLY running headless
+    //    options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+    //    options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+   //     options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+        options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+   //     options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+//disabled for temporaily bindu for debugging
+//options.addArguments("chrome.switches","--disable-extensions");
 
 ChromeDriverService driverService = new ChromeDriverService.Builder().usingDriverExecutable(new File("./Properties/chromedriver.exe")).usingAnyFreePort().build();
 	driverService.start();
